e33f5ff4a3a406e9aca635099205f6881b27f7ea
adriaauca <adriaac99@gmail.com>
2020-06-12 09:55:08 +0200

    understanding the base feature & syntax

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Person from './Person/Person'
 
+
 class App extends Component {
   state = {
     persons: [
@@ -18,15 +19,31 @@ class App extends Component {
     ]});
   }
 
+  nameChangeHandler = (event) => {
+    this.setState({persons: [
+      {name: event.target.value, age: 23},
+      {name: 'Pere', age: 29}
+    ]});
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button style={style} onClick={this.switchNameHandler}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age} 
           click={this.switchNameHandler}
+          changed={this.nameChangeHandler}
         />
         <Person 
           name={this.state.persons[1].name} 
